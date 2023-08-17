89fea422223ef647f96210c27d05484887285f71
Greeny <117624471+Greenydon198@users.noreply.github.com>
2023-08-17 15:05:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function threeSum(arr, target) {
 // write your code here
+	arr.sort();
+	let diff = Integer.MAX_VALUE;
+
+	for(let i=0;i<arr.length-2;i++){
+		let j = i+1;
+		let k = arr.length-1;
+		while(j<k){
+			let sum = arr[i] + arr[j] + arr[k];
+			if(sum==target)return target;
+			if(sum<target)j++;
+			else k--;
+			if(Math.abs(diff)>Math.abs(target-sum))
+				diff = target-sum;
+				// System.out.println(diff);
+		}
+	}
+	return target-diff;
   
 }
 
